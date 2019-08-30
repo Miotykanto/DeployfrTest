8708adf698eda84a4736fbf6de3ec1801727bef2
Miotykanto <47355711+Miotykanto@users.noreply.github.com>
2019-08-30 16:33:50 +0300

    Update Admin.js

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import "./Admin.css"
 
 class Admin extends Component {
     render() {
@@ -17,9 +18,9 @@ class Admin extends Component {
         <div class="navbar-collapse collapse">
             <ul class="nav navbar-nav navbar-right">
                 <li class="dropdown">
-                    <a class="dropdown-toggle" role="button" data-toggle="dropdown" href="#"><i class="fa fa-user-circle"></i> Admin <span class="caret"></span></a>
+                    <a class="dropdown-toggle" role="button" data-toggle="dropdown" href="#" id="daholo"><i class="fa fa-user-circle"></i> Admin <span class="caret"></span></a>
                     <ul id="g-account-menu" class="dropdown-menu" role="menu">
-                        <li><a href="#"><i class="fa fa-user-secret"></i> Mon  Profile</a></li>
+                        <li><a href="#"><i class="fa fa-user-secret"></i> My Profile</a></li>
                     </ul>
                 </li>
                 <li><a href="#"><i class="fa fa-sign-out"></i> Logout</a></li>
@@ -38,16 +39,193 @@ class Admin extends Component {
     <ul class="nav nav-pills nav-stacked" style="border-right:1px solid black">
         
         <li><a href="#"><i class="fa fa-dashboard"></i> Dashboard</a></li>
-        <li><a href="#"><i class="fa fa-tags"></i> Reference code</a></li>
-        <li><a href="#"><i class="fa fa-history"></i> Les histiore</a></li>
-        <li><a href="#"><i class="fa fa-lock"></i> changer le mot de passe</a></li>
+        <li><a href="#"><i class="fa fa-tags"></i> Referal Code</a></li>
+        <li><a href="#"><i class="fa fa-history"></i> Redeem History</a></li>
+        <li><a href="#"><i class="fa fa-lock"></i> Change Password</a></li>
 
     </ul>
 </div>
 <div class="col-lg-10 col-md-10 col-sm-9 col-xs-12">
     
 
-    <a href="#"><strong><span class="fa fa-dashboard"></span> Mon Dashboard</strong></a>
+    <a href="#"><strong><span class="fa fa-dashboard"></span> My Dashboard</strong></a>
+   
+</div>
+
+</div> 
+        );
+    }
+}
+
+export default Admin;import React, { Component } from 'react';
+import "./Admin.css"
+
+class Admin extends Component {
+    render() {
+        return (
+<div>   
+<div id="top-nav" class="navbar navbar-inverse navbar-static-top">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Dashboard</a>
+        </div>
+        <div class="navbar-collapse collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <li class="dropdown">
+                    <a class="dropdown-toggle" role="button" data-toggle="dropdown" href="#" id="daholo"><i class="fa fa-user-circle"></i> Admin <span class="caret"></span></a>
+                    <ul id="g-account-menu" class="dropdown-menu" role="menu">
+                        <li><a href="#"><i class="fa fa-user-secret"></i> My Profile</a></li>
+                    </ul>
+                </li>
+                <li><a href="#"><i class="fa fa-sign-out"></i> Logout</a></li>
+            </ul>
+        </div>
+    </div>
+   
+</div>
+
+
+
+
+
+<div class="col-lg-2 col-md-2 col-sm-3 col-xs-12">
+
+    <ul class="nav nav-pills nav-stacked" style="border-right:1px solid black">
+        
+        <li><a href="#"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+        <li><a href="#"><i class="fa fa-tags"></i> Referal Code</a></li>
+        <li><a href="#"><i class="fa fa-history"></i> Redeem History</a></li>
+        <li><a href="#"><i class="fa fa-lock"></i> Change Password</a></li>
+
+    </ul>
+</div>
+<div class="col-lg-10 col-md-10 col-sm-9 col-xs-12">
+    
+
+    <a href="#"><strong><span class="fa fa-dashboard"></span> My Dashboard</strong></a>
+   
+</div>
+
+</div> 
+        );
+    }
+}
+
+export default Admin;import React, { Component } from 'react';
+import "./Admin.css"
+
+class Admin extends Component {
+    render() {
+        return (
+<div>   
+<div id="top-nav" class="navbar navbar-inverse navbar-static-top">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Dashboard</a>
+        </div>
+        <div class="navbar-collapse collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <li class="dropdown">
+                    <a class="dropdown-toggle" role="button" data-toggle="dropdown" href="#" id="daholo"><i class="fa fa-user-circle"></i> Admin <span class="caret"></span></a>
+                    <ul id="g-account-menu" class="dropdown-menu" role="menu">
+                        <li><a href="#"><i class="fa fa-user-secret"></i> My Profile</a></li>
+                    </ul>
+                </li>
+                <li><a href="#"><i class="fa fa-sign-out"></i> Logout</a></li>
+            </ul>
+        </div>
+    </div>
+   
+</div>
+
+
+
+
+
+<div class="col-lg-2 col-md-2 col-sm-3 col-xs-12">
+
+    <ul class="nav nav-pills nav-stacked" style="border-right:1px solid black">
+        
+        <li><a href="#"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+        <li><a href="#"><i class="fa fa-tags"></i> Referal Code</a></li>
+        <li><a href="#"><i class="fa fa-history"></i> Redeem History</a></li>
+        <li><a href="#"><i class="fa fa-lock"></i> Change Password</a></li>
+
+    </ul>
+</div>
+<div class="col-lg-10 col-md-10 col-sm-9 col-xs-12">
+    
+
+    <a href="#"><strong><span class="fa fa-dashboard"></span> My Dashboard</strong></a>
+   
+</div>
+
+</div> 
+        );
+    }
+}
+
+export default Admin;import React, { Component } from 'react';
+import "./Admin.css"
+
+class Admin extends Component {
+    render() {
+        return (
+<div>   
+<div id="top-nav" class="navbar navbar-inverse navbar-static-top">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Dashboard</a>
+        </div>
+        <div class="navbar-collapse collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <li class="dropdown">
+                    <a class="dropdown-toggle" role="button" data-toggle="dropdown" href="#" id="daholo"><i class="fa fa-user-circle"></i> Admin <span class="caret"></span></a>
+                    <ul id="g-account-menu" class="dropdown-menu" role="menu">
+                        <li><a href="#"><i class="fa fa-user-secret"></i> My Profile</a></li>
+                    </ul>
+                </li>
+                <li><a href="#"><i class="fa fa-sign-out"></i> Logout</a></li>
+            </ul>
+        </div>
+    </div>
+   
+</div>
+
+
+
+
+
+<div class="col-lg-2 col-md-2 col-sm-3 col-xs-12">
+
+    <ul class="nav nav-pills nav-stacked" style="border-right:1px solid black">
+        
+        <li><a href="#"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+        <li><a href="#"><i class="fa fa-tags"></i> Referal Code</a></li>
+        <li><a href="#"><i class="fa fa-history"></i> Redeem History</a></li>
+        <li><a href="#"><i class="fa fa-lock"></i> Change Password</a></li>
+
+    </ul>
+</div>
+<div class="col-lg-10 col-md-10 col-sm-9 col-xs-12">
+    
+
+    <a href="#"><strong><span class="fa fa-dashboard"></span> My Dashboard</strong></a>
    
 </div>
 
