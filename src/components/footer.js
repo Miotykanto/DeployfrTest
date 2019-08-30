ecfd093ac10d7c5bdd584ac729b872573cef083a
Miotykanto <47355711+Miotykanto@users.noreply.github.com>
2019-08-30 17:01:35 +0300

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,50 +1,43 @@
 import React from "react";
 import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
-import './footer.css'
+import './footer.js'
 const FooterPage = () => {
   return (
-
-
-    <footer class="page-footer font-small cyan darken-3" id="tefy">
-
-  <div class="container">
-
-    <div class="row">
-
-      <div class="col-md-12 py-5">
-        <div class="mb-5 flex-center">
-
-          <a class="fb-ic">
-            <i class="fab fa-facebook-f fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
-          </a>
-
-          <a class="tw-ic">
-            <i class="fab fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
-          </a>
-
-          <a class="gplus-ic">
-            <i class="fab fa-google-plus-g fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
-          </a>
-
-         
-        </div>
+    <MDBFooter  className="font-small pt-4 mt-4 " id="foot">
+      <MDBContainer fluid className="text-center text-md-left" >
+        <MDBRow>
+          <MDBCol md="6">
+            <h5 className="title"></h5>
+            <p>
+              
+            </p>
+          </MDBCol>
+          <MDBCol md="6">
+            <h5 className="title">Links <br/>
+            </h5>
+            <ul>
+              <li className="list-unstyled">
+                <a href="#!"></a>
+              </li>
+              <li className="list-unstyled">
+                <a href="#!"></a>
+              </li>
+              <li className="list-unstyled">
+                <a href="#!"></a>
+              </li>
+              <li className="list-unstyled">
+                <a href="#!"></a>
+              </li>
+            </ul>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+      <div className="footer-copyright text-center py-3">
+        <MDBContainer fluid id='anarana'>
+          &copy; {new Date().getFullYear()}  <a className="anarana"> MIRANTO Tefimampihonona </a>
+        </MDBContainer>
       </div>
-
-
-    </div>
-
-
-  </div>
- 
-
-
-  <div class="footer-copyright text-center py-3">© 2018 Copyright:
-    <a href="https://github.com/Miotykanto/" id="anarana"> MIRANTO Tefimampihonona</a>
-  </div>
-
-
-</footer>
-
+    </MDBFooter>
   );
 }
 
