aa5207e5c4928095dbc591627572cabddd788b11
Miotykanto <47355711+Miotykanto@users.noreply.github.com>
2019-08-30 16:31:23 +0300

    Update authentication.js

--- src/actions/authentication.js
+++ src/actions/authentication.js
@@ -4,8 +4,7 @@ import setAuthToken from '../setAuthToken';
 import jwt_decode from 'jwt-decode';
 
 export const registerUser = (user, history) => dispatch => {
-    //axios.post('https://metyza-cuisine.herokuapp.com/register', user)
-    axios.post('https://io-back.herokuapp.com/register', user)
+    axios.post('https://ngambae.herokuapp.com/register', user)
             .then(res =>  history.push('/login')
                 )
             .catch(err => {
@@ -17,16 +16,159 @@ export const registerUser = (user, history) => dispatch => {
 }
 
 export const loginUser = (user) => dispatch => {
-    axios.post('https://io-back.herokuapp.com/login', user)
+    axios.post('https://ngambae.herokuapp.com/login', user)
             .then(res => {
                 const { token } = res.data;
                 localStorage.setItem('jwtToken', token);
                 localStorage.setItem('id', res.data.id);
                  console.log(res.data);
-                 localStorage.setItem('anarana',res.data.name)
-                 
-                 
-                console.log(res.data.name);
+                
+                setAuthToken(token);
+                const decoded = jwt_decode(token);
+                dispatch(setCurrentUser(decoded));
+            })
+            .catch(err => {
+                dispatch({
+                    type: GET_ERRORS,
+                    payload: err.response.data
+                });
+            });
+}
+
+export const setCurrentUser = decoded => {
+    return {
+        type: SET_CURRENT_USER,
+        payload: decoded
+    }
+}
+
+export const logoutUser = (history) => dispatch => {
+    localStorage.removeItem('jwtToken');
+    setAuthToken(false);
+    dispatch(setCurrentUser({}));
+    history.push('/login');
+}import axios from 'axios';
+import { GET_ERRORS, SET_CURRENT_USER } from './types';
+import setAuthToken from '../setAuthToken';
+import jwt_decode from 'jwt-decode';
+
+export const registerUser = (user, history) => dispatch => {
+    axios.post('https://ngambae.herokuapp.com/register', user)
+            .then(res =>  history.push('/login')
+                )
+            .catch(err => {
+                dispatch({
+                    type: GET_ERRORS,
+                    payload: err.response.data
+                });
+            });
+}
+
+export const loginUser = (user) => dispatch => {
+    axios.post('https://ngambae.herokuapp.com/login', user)
+            .then(res => {
+                const { token } = res.data;
+                localStorage.setItem('jwtToken', token);
+                localStorage.setItem('id', res.data.id);
+                 console.log(res.data);
+                
+                setAuthToken(token);
+                const decoded = jwt_decode(token);
+                dispatch(setCurrentUser(decoded));
+            })
+            .catch(err => {
+                dispatch({
+                    type: GET_ERRORS,
+                    payload: err.response.data
+                });
+            });
+}
+
+export const setCurrentUser = decoded => {
+    return {
+        type: SET_CURRENT_USER,
+        payload: decoded
+    }
+}
+
+export const logoutUser = (history) => dispatch => {
+    localStorage.removeItem('jwtToken');
+    setAuthToken(false);
+    dispatch(setCurrentUser({}));
+    history.push('/login');
+}import axios from 'axios';
+import { GET_ERRORS, SET_CURRENT_USER } from './types';
+import setAuthToken from '../setAuthToken';
+import jwt_decode from 'jwt-decode';
+
+export const registerUser = (user, history) => dispatch => {
+    axios.post('https://ngambae.herokuapp.com/register', user)
+            .then(res =>  history.push('/login')
+                )
+            .catch(err => {
+                dispatch({
+                    type: GET_ERRORS,
+                    payload: err.response.data
+                });
+            });
+}
+
+export const loginUser = (user) => dispatch => {
+    axios.post('https://ngambae.herokuapp.com/login', user)
+            .then(res => {
+                const { token } = res.data;
+                localStorage.setItem('jwtToken', token);
+                localStorage.setItem('id', res.data.id);
+                 console.log(res.data);
+                
+                setAuthToken(token);
+                const decoded = jwt_decode(token);
+                dispatch(setCurrentUser(decoded));
+            })
+            .catch(err => {
+                dispatch({
+                    type: GET_ERRORS,
+                    payload: err.response.data
+                });
+            });
+}
+
+export const setCurrentUser = decoded => {
+    return {
+        type: SET_CURRENT_USER,
+        payload: decoded
+    }
+}
+
+export const logoutUser = (history) => dispatch => {
+    localStorage.removeItem('jwtToken');
+    setAuthToken(false);
+    dispatch(setCurrentUser({}));
+    history.push('/login');
+}import axios from 'axios';
+import { GET_ERRORS, SET_CURRENT_USER } from './types';
+import setAuthToken from '../setAuthToken';
+import jwt_decode from 'jwt-decode';
+
+export const registerUser = (user, history) => dispatch => {
+    axios.post('https://ngambae.herokuapp.com/register', user)
+            .then(res =>  history.push('/login')
+                )
+            .catch(err => {
+                dispatch({
+                    type: GET_ERRORS,
+                    payload: err.response.data
+                });
+            });
+}
+
+export const loginUser = (user) => dispatch => {
+    axios.post('https://ngambae.herokuapp.com/login', user)
+            .then(res => {
+                const { token } = res.data;
+                localStorage.setItem('jwtToken', token);
+                localStorage.setItem('id', res.data.id);
+                 console.log(res.data);
                 
                 setAuthToken(token);
                 const decoded = jwt_decode(token);
